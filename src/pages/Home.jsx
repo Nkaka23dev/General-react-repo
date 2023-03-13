ee7ada1f508f5847a2e1569079ecc5d8c1cadc99
Nkaka23dev <nkakaeric96@gmail.com>
2023-03-13 12:47:08 +0200

    feat: basics in routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-4xl text-center text-white bg-gray-800'>This is the home</div>
+  )
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='h-screen grid'>
+        <h1 className='m-auto text-5xl '>Page Not Found 404!</h1>
+    </div>
+  )
+}
