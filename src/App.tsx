04583df62cab82889d45c12cb38a151bd80f8370
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-16 09:51:10 +0200

    feat: finish dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import Person, { COUNTRY } from './components/Person'
-
 function App() {
   return (
-    <Person
-      name='Eric Nkaka'
-      email='nkakaeric96@gmail.com'
-      isMarried={false}
-      friends={["Isheja", "Eric", "Will Shyka", "Yvette Manzi"]}
-      country={COUNTRY.RWANDA} />
+    <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+      <div>
+        <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+          <div className="text-white font-bold">Icon</div>
+        </span>
+      </div>
+      <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
+      <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+      </p>
+    </div>
+
   )
 }
 
