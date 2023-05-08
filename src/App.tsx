6b8f17ee87505bc831979eb631777b4cbf0de284
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-08 15:40:18 +0300

    feat: finish useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import axios from "axios";
+import { useEffect, useState, useMemo } from "react"
+
 function App() {
+  const [data, setData] = useState<any>();
+  const [toggle, setToggle] = useState<boolean>(true);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+      .then((result: any) => setData(result.data))
+    console.log("USEEEFECFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
+  }, []);
+
+  const findTheLongestName = (comments: any) => {
+    if (!comments) return null;
+    let longestName = ""
+    for (let i = 0; i < comments.length; i++) {
+      let currentName = comments[i].name;
+      if (currentName.length > longestName.length) {
+        longestName = currentName;
+      }
+    }
+    console.log("<============== I AM BEING CALLED ============>")
+    return longestName;
+  }
+  const getTheLongestName = useMemo(() => findTheLongestName(data), [data]);
+  
   return (
-    <h1 className="text-center">React Table start here</h1>
+    <div className="grid">
+      <h1 className="text-center">React UseMemo Hook <span className="text-xl text-red-500">{getTheLongestName}</span> </h1>
+      <button onClick={() => setToggle(!toggle)}
+        className="py-3 mx-auto px-5 bg-sky-500 text-white text-lg font-semibold">
+        {toggle ? "Show" : "Hidden"}
+      </button>
+    </div>
   )
 }
 
