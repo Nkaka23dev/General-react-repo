cab35ddb698a116ecb64d5fc161574083692b651
Nkaka23dev <nkakaeric96@gmail.com>
2023-05-15 14:35:52 +0300

    feat: finish use mome

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,33 @@
-import axios from "axios";
-import { useEffect, useState, useMemo } from "react"
+import { useMemo, useState } from "react"
 
 function App() {
-  const [data, setData] = useState<any>();
-  const [toggle, setToggle] = useState<boolean>(true);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/comments")
-      .then((result: any) => setData(result.data))
-    console.log("USEEEFECFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
-  }, []);
-
-  const findTheLongestName = (comments: any) => {
-    if (!comments) return null;
-    let longestName = ""
-    for (let i = 0; i < comments.length; i++) {
-      let currentName = comments[i].name;
-      if (currentName.length > longestName.length) {
-        longestName = currentName;
-      }
-    }
-    console.log("<============== I AM BEING CALLED ============>")
-    return longestName;
+  const incrementCountOne = () => {
+    setCount1(prev => prev + 1)
   }
-  const getTheLongestName = useMemo(() => findTheLongestName(data), [data]);
-  
+
+
+  const isEven = () => {
+    let value;
+    setTimeout(() => {
+      value = count1 % 2 === 0;
+    }, 60000);
+    return value
+  }
+
+  const incrementCountTwo = () => {
+    setCount2(prev => prev + 1)
+  }
+
   return (
-    <div className="grid">
-      <h1 className="text-center">React UseMemo Hook <span className="text-xl text-red-500">{getTheLongestName}</span> </h1>
-      <button onClick={() => setToggle(!toggle)}
-        className="py-3 mx-auto px-5 bg-sky-500 text-white text-lg font-semibold">
-        {toggle ? "Show" : "Hidden"}
-      </button>
+    <div className="grid max-w-xl mx-auto py-10 space-y-5 px-3">
+      <button onClick={incrementCountOne} className="py-3 px-10 text-white bg-green-500
+       hover:bg-green-600">Count One  -  <span className="text-xl">{count1}</span></button>
+      <h1>{isEven() ? count1 : ''}</h1>
+      <button onClick={incrementCountTwo} className="py-3 px-10 text-white bg-yellow-500
+       hover:bg-yellow-600">Count One  -  <span className="text-xl">{count2}</span></button>
     </div>
   )
 }
